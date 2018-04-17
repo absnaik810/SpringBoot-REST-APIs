048735168dc076ad6092e87ccda5c69a48914cb3
Abhishek Naik <abhisheknaik@149-161-144-97.dhcp-bl.indiana.edu>
2018-04-17 18:22:58 -0400

    Using JPA & database

--- src/main/java/me/abhisheknaik/SpringBootRESTAPI/Blog.java
+++ src/main/java/me/abhisheknaik/SpringBootRESTAPI/Blog.java
@@ -1,11 +1,24 @@
 package me.abhisheknaik.SpringBootRESTAPI;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Blog {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+	
 	private String title;
 	private String content;
 	
-	public Blog() {
+	public Blog() {}
+	
+	public Blog(String title, String content) {
+		this.setTitle(title);
+		this.setContent(content);
 	}
 	
 	public Blog(int id, String title, String content) {
@@ -18,20 +31,20 @@ public class Blog {
 		return id;
 	}
 	
-	public void setId(int id) {
-		this.id=id;
-	}
-	
 	public String getTitle() {
 		return title;
 	}
 	
-	public void setTitle(String title) {
-		this.title=title;
-	}
-	
 	public String getContent() {
 		return content;
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public void setTitle(String title) {
+		this.title=title;
 	}
 	
 	public void setContent(String content) {
@@ -41,9 +54,9 @@ public class Blog {
 	@Override
 	public String toString() {
 		return "Blog{" + 
-				"id=" + id +
+				"id=" + id +  
 				", title='" + title + '\'' +
-				", content=" + content + '\'' +
-				'}'; 
+				", content='" + content + '\'' + 
+				'}';
 	}
 }
---
+++ src/main/java/me/abhisheknaik/SpringBootRESTAPI/BlogRepository.java
@@ -0,0 +1,11 @@
+package me.abhisheknaik.SpringBootRESTAPI;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
+	List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+}
